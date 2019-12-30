9c114ce1cc3df1526a4371d997c314437b02fe22
Matthew Beckerleg <ord.sc2@gmail.com>
2019-12-30 11:31:29 -0800

    installed faker

--- schema.sql
+++ schema.sql
@@ -13,3 +13,18 @@ CREATE TABLE homes (
     votes             INT NOT NULL,
     PRIMARY KEY(id)
 );
+
+
+-- mine: 
+
+-- CREATE TABLE homes (
+--     id                SERIAL PRIMARY KEY,
+--     Listing_id        VARCHAR(3),
+--     image             TEXT,
+--     house_type        VARCHAR(25),
+--     location          VARCHAR(100),
+--     description       VARCHAR(100),
+--     cost_per_night    INT NOT NULL,
+--     rating            DECIMAL(5,2),
+--     votes             INT NOT NULL,
+-- );
