f73b8c480d47425697b381c25cc6c664407b4cc2
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-06 11:45:37 -0800

    pared down package.json and related

--- schema.sql
+++ schema.sql
@@ -1,9 +1,6 @@
--- ps:
 CREATE DATABASE fairtest;
 
-\c fairtest;
-
-CREATE TABLE homes (
+CREATE TABLE fairtest.homes (
     id                SERIAL PRIMARY KEY,
     image             TEXT,
     house_type        VARCHAR(25),
@@ -13,22 +10,3 @@ CREATE TABLE homes (
     rating            DECIMAL(5,2),
     votes             INT NOT NULL
 );
-
-
--- mongo: 
-
--- DROP DATABASE IF EXISTS more_homes;
--- CREATE DATABASE more_homes;
--- USE more_homes;
-
--- CREATE TABLE homes (
---     id                INT AUTO_INCREMENT,
---     img               VARCHAR(250),
---     house_type        VARCHAR(25),
---     location          VARCHAR(100),
---     description       VARCHAR(100),
---     cost_per_night    INT NOT NULL,
---     rating            DECIMAL(5,2),
---     votes             INT NOT NULL,
---     PRIMARY KEY(id)
--- );
