d2d15c30f76539d0cdf1cf563b4544c1c37e34ed
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-07 11:50:13 -0800

    uploading onto ec2 soon

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,15 @@
-// const pg = require('pg');
-// const config = require('./config.js');
+const pg = require('pg');
+const config = require('./config.js');
 
-// const conn = new pg.Pool(config);
-
-// module.exports = conn;
-
-const { Pool } = require('pg');
-
-const conn = new Pool({
-  host: 'localhost',
-  database: 'fairtest',
-});
+const conn = new pg.Pool(config);
 
 module.exports = conn;
+
+// const { Pool } = require('pg');
+
+// const conn = new Pool({
+//   host: 'localhost',
+//   database: 'fairtest',
+// });
+
+// module.exports = conn;
