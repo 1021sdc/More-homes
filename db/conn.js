f8e6661f21a5ae9e132a4b90fdb8fe0bbdd35f9f
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-07 17:05:53 -0800

    working towards integrating process.env

--- db/conn.js
+++ db/conn.js
@@ -4,12 +4,3 @@ const config = require('./config.js');
 const conn = new pg.Pool(config);
 
 module.exports = conn;
-
-// const { Pool } = require('pg');
-
-// const conn = new Pool({
-//   host: 'localhost',
-//   database: 'fairtest',
-// });
-
-// module.exports = conn;
